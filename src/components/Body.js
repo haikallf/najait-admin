fd5824e5e5351f62da3915ac163b27a2025ae253
haikallf <haikallzrd@gmail.com>
2021-12-05 02:22:18 +0700

    nambah halaman penjahit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Body.css";
+import Penjahit from "./Penjahit";
 
 function Body() {
-  return <div className="body">Body</div>;
+  return (
+    <div className="body">
+      <Penjahit />
+    </div>
+  );
 }
 
 export default Body;
