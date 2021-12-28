8baed95e93f60e01bc166e0be303f9644d42b6e6
haikallf <haikallzrd@gmail.com>
2021-12-28 17:12:45 +0700

    accept modal

---
+++ src/components/AcceptModal.js
@@ -0,0 +1,87 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { Avatar, MenuItem, TextField } from "@mui/material";
+import "./AcceptModal.css";
+import { styled } from "@mui/material/styles";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #266679",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+  outline: "none",
+};
+
+export default function AccepttModal({
+  open,
+  handleOpen,
+  handleClose,
+  penjahit,
+}) {
+  const TealTextField = styled(TextField)({
+    "& label.Mui-focused": {
+      color: "#266679",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#266679",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#266679",
+      },
+      "&:hover fieldset": {
+        borderColor: "#266679",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#266679",
+      },
+    },
+  });
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="acceptModal__container">
+            <TealTextField
+              fullWidth
+              label="Status"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              select
+            >
+              <MenuItem value="AVAILABLE">Available</MenuItem>
+              <MenuItem value="NOT AVAILABLE">Not Available</MenuItem>
+            </TealTextField>
+
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#266679",
+                width: "100px",
+                height: "56px",
+                marginLeft: "10px",
+              }}
+            >
+              Accept
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/components/Pesanan.js
+++ src/components/Pesanan.js
@@ -11,6 +11,7 @@ import TableRow from "@mui/material/TableRow";
 import { Button } from "@mui/material";
 import axios from "axios";
 import { url } from "../globalConfig";
+import AccepttModal from "./AcceptModal";
 
 const rows = [
   {
@@ -18,6 +19,7 @@ const rows = [
     id_penjahit: 1,
     id_user: 1,
     jenis: "Kemeja",
+    pakaian: "Kemeja",
     catatan: "Potong 2cm",
     waktu_pesan: "2021-21-21",
     status: "pending",
@@ -27,6 +29,7 @@ const rows = [
     id_penjahit: 1,
     id_user: 1,
     jenis: "Kemeja",
+    pakaian: "Kemeja",
     catatan: "Potong 2cm",
     waktu_pesan: "2021-21-21",
     status: "pending",
@@ -36,6 +39,7 @@ const rows = [
     id_penjahit: 1,
     id_user: 1,
     jenis: "Kemeja",
+    pakaian: "Kemeja",
     catatan: "Potong 2cm",
     waktu_pesan: "2021-21-21",
     status: "ongoing",
@@ -45,6 +49,7 @@ const rows = [
     id_penjahit: 1,
     id_user: 1,
     jenis: "Kemeja",
+    pakaian: "Kemeja",
     catatan: "Potong 2cm",
     waktu_pesan: "2021-21-21",
     status: "ongoing",
@@ -54,6 +59,7 @@ const rows = [
     id_penjahit: 1,
     id_user: 1,
     jenis: "Kemeja",
+    pakaian: "Kemeja",
     catatan: "Potong 2cm",
     waktu_pesan: "2021-21-21",
     status: "ongoing",
@@ -63,6 +69,7 @@ const rows = [
     id_penjahit: 1,
     id_user: 1,
     jenis: "Kemeja",
+    pakaian: "Kemeja",
     catatan: "Potong 2cm",
     waktu_pesan: "2021-21-21",
     status: "pending",
@@ -73,6 +80,13 @@ export default function Pesanan() {
   const [page, setPage] = useState(0);
   // const [rows, setRows] = useState([]);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+  const [currentPenjahit, setCurrentPenjahit] = React.useState({});
 
   // useEffect(() => {
   //   getBahan();
@@ -96,84 +110,96 @@ export default function Pesanan() {
     rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
-    <div className="penjahit">
-      <div className="penjahit__up">
-        <div className="penjahit__title">PESANAN</div>
-      </div>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer sx={{ maxHeight: "80vh" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">ID Pesanan</TableCell>
-                <TableCell align="center">ID Penjahit</TableCell>
-                <TableCell align="center">ID User</TableCell>
-                <TableCell align="center">Jenis</TableCell>
-                <TableCell align="center">Catatan</TableCell>
-                <TableCell align="center">Waktu Pesan</TableCell>
-                <TableCell align="center">Status</TableCell>
-                <TableCell align="center">Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows
-                ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => (
-                  <TableRow key={row.name}>
-                    <TableCell align="center">{row.id_order}</TableCell>
-                    <TableCell align="center">{row.id_penjahit}</TableCell>
-                    <TableCell align="center">{row.id_user}</TableCell>
-                    <TableCell align="center">{row.jenis}</TableCell>
-                    <TableCell align="center">{row.catatan}</TableCell>
-                    <TableCell align="center">{row.waktu_pesan}</TableCell>
-                    <TableCell align="center">{row.status}</TableCell>
-                    <TableCell align="center">
-                      {row.status == "pending" ? (
-                        <Button
-                          variant="outlined"
-                          style={{
-                            borderColor: "#4abdac",
-                            color: "#4abdac",
-                            width: "90px",
-                            height: "40px",
-                          }}
-                        >
-                          Accept
-                        </Button>
-                      ) : (
-                        <Button
-                          variant="outlined"
-                          style={{
-                            borderColor: "#4abdac",
-                            color: "#4abdac",
-                            width: "90px",
-                            height: "40px",
-                          }}
-                        >
-                          Edit
-                        </Button>
-                      )}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 53 * emptyRows }}>
-                  <TableCell colSpan={6} />
+    <>
+      <AccepttModal
+        open={open}
+        handleOpen={handleOpen}
+        handleClose={handleClose}
+        penjahit={currentPenjahit}
+      />
+
+      <div className="penjahit">
+        <div className="penjahit__up">
+          <div className="penjahit__title">PESANAN</div>
+        </div>
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: "80vh" }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">ID Pesanan</TableCell>
+                  <TableCell align="center">ID Penjahit</TableCell>
+                  <TableCell align="center">ID User</TableCell>
+                  <TableCell align="center">Jenis</TableCell>
+                  <TableCell align="center">Pakaian</TableCell>
+                  <TableCell align="center">Catatan</TableCell>
+                  <TableCell align="center">Waktu Pesan</TableCell>
+                  <TableCell align="center">Status</TableCell>
+                  <TableCell align="center">Action</TableCell>
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </div>
+              </TableHead>
+              <TableBody>
+                {rows
+                  ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index) => (
+                    <TableRow key={row.name}>
+                      <TableCell align="center">{row.id_order}</TableCell>
+                      <TableCell align="center">{row.id_penjahit}</TableCell>
+                      <TableCell align="center">{row.id_user}</TableCell>
+                      <TableCell align="center">{row.jenis}</TableCell>
+                      <TableCell align="center">{row.pakaian}</TableCell>
+                      <TableCell align="center">{row.catatan}</TableCell>
+                      <TableCell align="center">{row.waktu_pesan}</TableCell>
+                      <TableCell align="center">{row.status}</TableCell>
+                      <TableCell align="center">
+                        {row.status == "pending" ? (
+                          <Button
+                            onClick={handleOpen}
+                            variant="outlined"
+                            style={{
+                              borderColor: "#4abdac",
+                              color: "#4abdac",
+                              width: "90px",
+                              height: "40px",
+                            }}
+                          >
+                            Accept
+                          </Button>
+                        ) : (
+                          <Button
+                            variant="outlined"
+                            style={{
+                              borderColor: "#4abdac",
+                              color: "#4abdac",
+                              width: "90px",
+                              height: "40px",
+                            }}
+                          >
+                            Edit
+                          </Button>
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </div>
+    </>
   );
 }
